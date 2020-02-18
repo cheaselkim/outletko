d62723a58c3d31996bb02cac7ea97fd8d8cf4645
cheaselkim <dooleycheasel@gmail.com>
2020-02-18 13:50:23 +0800

    Update blog design

--- application/views/website/header.php
+++ application/views/website/header.php
@@ -1,14 +1,12 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/login3.css') ?>">
 
-<nav class="navbar navbar-expand-md " style="height: 70px;background: rgb(79, 98, 40);">
-	<a class="navbar-brand font-small" href="<?php echo base_url() ?>"><img src="<?php echo base_url('assets/img/logo-10.png')?>" class="border img-header-website" alt=""></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-		<span class="fas fa-bars " style="color: orange;"></span>
-	</button>
-	<div class="collapse navbar-collapse justify-content-end " id="collapsibleNavbar" >
-
+<nav class="navbar navbar-expand-md " style="height: 70px;background: rgb(192, 219, 136);">
+	<div class="collapse navbar-collapse justify-content-start " id="collapsibleNavbar" >
 		<ul class="navbar-nav">
-			<li class="nav-item" >
+		<li class="nav-item">
+			<button class="btn text-white" style="background:rgb(79, 98, 40);">Entrepreneur Blog Articles</button>
+		</li>
+			<!-- <li class="nav-item" >
 				<a class="nav-link font-small" href="<?php echo base_url('aboutus') ?>"><span class="text-white font-bahaus-93">About Us</span></a>
 			</li>
 			<li class="nav-item" >
@@ -25,9 +23,15 @@
 			</li>
 			<li class="nav-item" >
 				<a class="nav-link font-small" href="<?php echo base_url('blog') ?>"><span class="text-white font-bahaus-93">Blog</span></a>
-			</li>
+			</li> -->
 		</ul>
 	</div>  
+
+	<span class="font-size-25 mt-3 font-weight-600" style="text-shadow: 2px 2px rgb(169,169,169);color:rgb(79, 98, 40); ">A big business starts small. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+	<a class="navbar-brand font-small" href="<?php echo base_url() ?>"><img src="<?php echo base_url('assets/img/logo-10.png')?>" class="border img-header-website" alt=""></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+		<span class="fas fa-bars " style="color: orange;"></span>
+	</button>
 </nav>
 
 <!-- END NAVBAR -->
